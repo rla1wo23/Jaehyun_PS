4378da23d40c3bf29ba872a6df0c7a9b03dd399b
rla1wo23@gmail.com <rla1wo23@gmail.com>
2024-08-15 10:08:19 +0900

    Update .gitignore to ignore .vscode and tmpCodeRunner

---
+++ acmicpc/10830.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int N, B;
+long long origin[5][5];
+long long tmp[5][5];
+int main() {
+  cin >> N >> B;
+  for (int i = 0; i < N; i++) {
+    for (int j = 0; j < N; j++) {
+      cin >> origin[i][j];
+      tmp[i][j] = origin[i][j];
+    }
+  }
+  for (int i = 0; i < B; i++) {
+    for(int x=0;)
+  }
+}
---
+++ leetcode/2334.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+class Solution {
+public:
+  int validSubarraySize(vector<int> &nums, int threshold) {}
+};
